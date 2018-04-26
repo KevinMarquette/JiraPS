b640dc9dc6d868f7cfc2cbcd64f4462f0ddfeaf6
Michael Dejulia <MDejulia@loandepot.com>
2018-04-26 10:08:15 -0700

    Add Types Import

--- JiraPS/JiraPS.psm1
+++ JiraPS/JiraPS.psm1
@@ -7,12 +7,34 @@
 $PublicFunctions = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
 $PrivateFunctions = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
+# Import Types
+try
+{
+    Add-Type -Path (Join-Path $PSScriptRoot JiraPS.Types.cs) -ReferencedAssemblies Microsoft.CSharp
+}
+catch
+{
+    if (!(("JiraPS.AssigneeType" -as [Type])))
+    {
+        $errorMessage = @{
+            Category         = "OperationStopped"
+            CategoryActivity = "Loading custom classes"
+            ErrorId          = 1001
+            Message          = "Failed to load module JiraPS. [Could not import JiraPS classes]"
+        }
+        Write-Error @errorMessage
+    }
+}
+
 # Dot source the functions
-foreach ($File in @($PublicFunctions + $PrivateFunctions)) {
-    Try {
+foreach ($File in @($PublicFunctions + $PrivateFunctions))
+{
+    Try
+    {
         . $File.FullName
     }
-    Catch {
+    Catch
+    {
         $errorItem = [System.Management.Automation.ErrorRecord]::new(
             ([System.ArgumentException]"Function not found"),
             'Load.Function',
